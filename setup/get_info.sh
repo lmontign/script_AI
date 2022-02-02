98ae341a28ca841049c17d8ab2f071c3e551ac31
Laurent Montigny <95751496+lmontign@users.noreply.github.com>
2022-02-02 16:06:42 +0100

    Create get_info.sh

---
+++ setup/get_info.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+exec > system_info.txt
+exec 2>&1
+
+# Host
+hostname
+echo -en '\n'
+
+uname -a
+echo -en '\n'
+
+sudo dmidecode -t bios -q
+echo -en '\n'
+
+lscpu
+echo -en '\n'
+
+lsblk
+echo -en '\n'
+
+cat /proc/meminfo
+echo -en '\n'
+
+
+# GPU
+dpkg -l | grep cuda
+echo -en '\n'
+
+dpkg -l | grep rocm
+echo -en '\n'
+
+nvidia-smi
+echo -en '\n'
+
+rocm-smi
+echo -en '\n'
+
+cat /proc/driver/nvidia/version
+echo -en '\n'
+
+
+# AI
+python --version
+python -c "print('Pytorch=');import torch; print(torch.__version__)"
+python3 -c "print('Tensorflow=');import tensorflow as tf; print(tf.__version__)"
