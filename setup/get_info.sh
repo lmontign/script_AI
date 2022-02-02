2a1168252ed73acdaeefc99bd9559c32fcfed8f2
Laurent Montigny <95751496+lmontign@users.noreply.github.com>
2022-02-02 16:08:28 +0100

    Update get_info.sh

--- setup/get_info.sh
+++ setup/get_info.sh
@@ -3,40 +3,19 @@ exec > system_info.txt
 exec 2>&1
 
 # Host
-hostname
-echo -en '\n'
-
-uname -a
-echo -en '\n'
-
-sudo dmidecode -t bios -q
-echo -en '\n'
-
-lscpu
-echo -en '\n'
-
-lsblk
-echo -en '\n'
-
-cat /proc/meminfo
-echo -en '\n'
-
+hostname && echo -en '\n'
+uname -a && echo -en '\n'
+sudo dmidecode -t bios -q && echo -en '\n'
+lscpu && echo -en '\n'
+lsblk && echo -en '\n'
+cat /proc/meminfo && echo -en '\n'
 
 # GPU
-dpkg -l | grep cuda
-echo -en '\n'
-
-dpkg -l | grep rocm
-echo -en '\n'
-
-nvidia-smi
-echo -en '\n'
-
-rocm-smi
-echo -en '\n'
-
-cat /proc/driver/nvidia/version
-echo -en '\n'
+dpkg -l | grep cuda && echo -en '\n'
+dpkg -l | grep rocm && echo -en '\n'
+nvidia-smi && echo -en '\n'
+rocm-smi && echo -en '\n'
+cat /proc/driver/nvidia/version && echo -en '\n'
 
 
 # AI
